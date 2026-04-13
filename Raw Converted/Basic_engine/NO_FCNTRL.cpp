9404fdad77a87720f7e126426cffbc534dd5694c
sourajdewalia <sourajdewalia@gmail.com>
2026-04-12 23:36:45 -0400

    Remove all converted C++ code to restart conversion from scratch
    Deleted MAIN.cpp, Raw Converted/, Code_as_of_22nd_July/, and both
    eclipse-workspace projects (CPP2, CPP3). MAIN.FOR corrections are
    preserved in git stash.

--- Raw Converted/Basic_engine/NO_FCNTRL.cpp
+++
@@ -1,9 +0,0 @@
-#include <fem.hpp> // Fortran EMulation library of fable module
-
-namespace BASIC_ENG {
-
-using namespace fem::major_types;
-
-using fem::common;
-
-} // namespace BASIC_ENG
--- eclipse-workspace/dyngen_iitb_CPP3/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l/home/souraj/dyngen_iitb/eclipse-workspace/dyngen_iitb_CPP3/fem -l/home/souraj/dyngen_iitb/eclipse-workspace/dyngen_iitb_CPP3/tbxx
-
